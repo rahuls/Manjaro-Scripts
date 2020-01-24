e4dd9f36212d689591dd0d2fd71f6666c418819a
Rahul <sudhanaboina.rahul@gmail.com>
2020-01-24 21:33:23 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -8,3 +8,6 @@ curl -L http://install.ohmyz.sh | sh
 
 #set default shell to zsh
 chsh -s /bin/zsh
+
+#Follow this instructin for plugins
+#https://gist.github.com/dogrocker/1efb8fd9427779c827058f873b94df95
