5ae5052f0902a9a7c5d2f4a8365cce600feb9fd1
Rahul <sudhanaboina.rahul@gmail.com>
2020-02-03 19:19:40 +0530

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -11,3 +11,4 @@ chsh -s /bin/zsh
 
 #Follow this instructin for plugins
 #https://gist.github.com/dogrocker/1efb8fd9427779c827058f873b94df95
+#https://github.com/popstas/zsh-command-time    Command time plugin calculates time taken by long commands.
